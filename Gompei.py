09edf1918578e0c10a58d4fa9fe30ed7141d0e19
SamuelCurrid <samuelcurrid@gmail.com>
2020-02-13 20:57:07 -0500

    Added prefix command

--- Gompei.py
+++ Gompei.py
@@ -1,13 +1,19 @@
 import json
 import os
 import discord
-import requests
 from discord.ext import commands
 
 from Leaderboards import Leaderboards
 from Settings import Settings
 
-gompei = commands.Bot(command_prefix=".")
+settings = {}
+
+
+def get_prefix(client, message):
+	return settings[str(message.guild.id)]["prefix"]
+
+
+gompei = commands.Bot(command_prefix=get_prefix)
 
 # Extensions
 gompei.add_cog(Leaderboards(gompei))
@@ -22,11 +28,27 @@ async def on_ready():
 	"""
 	Load state and update information since last run
 	"""
-
+	await load_state()
 	await gompei.change_presence(activity=discord.Game(name="Underwater Hockey"))
 	await update_guilds(gompei)
 
 	print("Logged on as {0}".format(gompei.user))
+
+
+async def load_state():
+	global settings
+
+	with open(os.path.join("config", "settings.json"), "r+") as settingsFile:
+		settings = json.loads(settingsFile.read())
+
+
+async def update_state():
+	global settings
+
+	with open(os.path.join("config", "settings.json"), "r+") as settingsFile:
+		settingsFile.truncate(0)
+		settingsFile.seek(0)
+		json.dump(settings, settingsFile, indent=4)
 
 
 @gompei.command(pass_context=True)
@@ -36,9 +58,9 @@ async def help(ctx):
 	"""
 
 	helpEmbed = discord.Embed(title="Gompei Bot", colour=discord.Colour.red())
-	helpEmbed.add_field(name="Documentation", value="https://samuelcurrid.github.io/Gompei-Bot/")
+	helpEmbed.add_field(name="Documentation", value="https://samuelcurrid.github.io/Gompei-Bot/documentation.html")
 	helpEmbed.set_thumbnail(url="https://raw.githubusercontent.com/SamuelCurrid/Gompei-Bot/master/assets/gompei.png")
-	helpEmbed.set_footer(text="Source: https://github.com/SamuelCurrid/Gompei-Bot")
+	helpEmbed.set_footer(text="Source: https://github.com/SamuelCurrid/Gompei-Bot/")
 
 	await ctx.message.channel.send(embed=helpEmbed)
 
@@ -47,29 +69,36 @@ async def update_guilds(self):
 	"""
 	Updates guilds included in leaderboards.json
 	"""
+	global settings
 
-	# with open(os.path.join("config", "settings"), "r+") as leaderboards:
-	# 	guilds = json.loads(leaderboards.read())
-	#
-	# 	savedGuilds = []
-	# 	for guildID in guilds:
-	# 		savedGuilds.append(int(guildID))
-	#
-	# 	guilds = []
-	# 	for guild in gompei.guilds:
-	# 		guilds.append(guild.id)
-	#
-	# 	addGuilds = [x for x in guilds if x not in savedGuilds]
-	# 	removeGuilds = [x for x in savedGuilds if x not in guilds]
-	#
-	# 	# Add new guilds
-	# 	for guildID in addGuilds:
-	# 		self.leaderboards[str(guildID)] = {"prefix": "."}
+	savedGuilds = []
+	for guildID in settings:
+		savedGuilds.append(int(guildID))
+
+	guilds = []
+	for guild in gompei.guilds:
+		guilds.append(guild.id)
+
+	addGuilds = [x for x in guilds if x not in savedGuilds]
+	removeGuilds = [x for x in savedGuilds if x not in guilds]
+
+	# Add new guilds
+	for guildID in addGuilds:
+		settings[str(guildID)] = {"prefix": "."}
 
 	# Remove disconnected guilds
-	# for guildID in removeGuilds:
-	# 	self.leaderboards.pop(str(guildID))
-	return
+	for guildID in removeGuilds:
+		settings.pop(str(guildID))
+
+	await update_state()
+
+
+@gompei.command(pass_context=True, name="prefix")
+async def change_prefix(ctx, prefix):
+	if ctx.message.author.guild_permissions.administrator:
+		settings[str(ctx.message.guild.id)]["prefix"] = str(prefix)
+
+		await update_state()
 
 
 gompei.run(json.load(open(os.path.join("config", "tokens.json")))["token"])
